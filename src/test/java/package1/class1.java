ad9e46beafdb9d168555e45bab9832030e3e471c
SUPREETHAJAYARAMgowda <s4supritha.gowda@gmail.com>
2024-04-03 10:16:24 +0530

    saved

---
+++ src/test/java/package1/class1.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class class1 {
+
+	public class1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
